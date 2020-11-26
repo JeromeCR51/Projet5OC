e89a055e432edb1054082b1cec2e8045457e2bbd
jeromeCR <jeromecrevel@sfr.fr>
2020-11-26 08:58:02 +0100

    1ere evolution

--- index.js
+++ index.js
@@ -6,53 +6,49 @@ request.onreadystatechange = function() {
         var cameraList = JSON.parse(this.responseText);
         console.log(cameraList);
 
+//        let camera = cameraList[0];
+let bodyElt = document.getElementsByTagName("body")[0];   // je récupère l'élément DOM body //
+
+let headerElt = document.getElementsByTagName("header")[0]; // je récupère l'élément DOM header //
+let titreHeader = document.createElement("H1"); // je crée une balise h1  // 
+let textTitrePrincipal = document.createTextNode("ORINOCO"); // je crée  un bloc de texte contenant le titre du header //
+    headerElt.appendChild(titreHeader); // J'attache la balise H1 à l 'header // 
+    titreHeader.appendChild(textTitrePrincipal);  // j'attache le texte à la balise H1 //
+
+let mainElt = document.createElement("main"); // je créé la balise main //
+
+bodyElt.appendChild(mainElt); // j'ajoute la balise main au body //
        
-
-       
-        let camera = cameraList[0];
-        let bodyElt = document.getElementsByTagName("body")[0];   // je récupère l'élément DOM body //
-
-        let headerElt = document.getElementsByTagName("header")[0]; // je récupère l'élément DOM header //
-        let titreHeader = document.createElement("H1"); // je crée une balise h1  // 
-        let textTitrePrincipal = document.createTextNode("Les Cameras"); // je crée  un bloc de texte contenant le titre du header //
-            titreHeader.appendChild(textTitrePrincipal);  // j'attache le texte à la balise H1 //
-            headerElt.appendChild(titreHeader); // J'attache la balise H1 à l 'header // 
-
       
-      
-      
-            for (let camera of cameraList) { // Boucle pour chaque camera  présente dans la liste des cameras //
-            const newArticleElt = document.createElement("div"); // je crée une balise div //
+for (let camera of cameraList) { // Boucle pour chaque camera  présente dans la liste des cameras //
+    const newArticleElt = document.createElement("div"); // je crée une balise div //
             newArticleElt.className = "listcameras"; // je crée une classe dans la balise div //
-            let titreclasslistcameras = document.createElement("H2"); // je crée une balise h2  // 
-            
-            let titreH2 = document.createTextNode(camera.name);   // je crée  un bloc de texte contenant le titre du h2 //
-            let figure =  document.createElement("figure"); // je crée une balise figure //
-            let hrefImage = document.createElement("a"); // je crée une balise <a> // 
-            let articleImage = document.createElement("IMG"); // je crée une balise image //
-            articleImage.src = camera.imageUrl; // je définis la propriéré SRC de l'image avec le contenu de l'imageUrl //
-            hrefImage.href = "/api/camera/" + camera._id;
-            let figcaption = document.createElement("figcaption"); // je crée la balise figcaption //
-            let figcaptionpara = document.createElement('p'); // je crée la balise paragraphe //
-            let articlePrice = document.createTextNode("prix" + " " + camera.price); // je crée un bloc texte contenant le prix de la camera //
-        
+    let titreclasslistcameras = document.createElement("H2"); // je crée une balise h2  // 
+    let titreH2 = document.createTextNode(camera.name);   // je crée  un bloc de texte contenant le titre du h2 //
+
+    let figure =  document.createElement("figure"); // je crée une balise figure //
+    let hrefImage = document.createElement("a"); // je crée une balise <a> // 
+    let articleImage = document.createElement("IMG"); // je crée une balise image //
+        articleImage.src = camera.imageUrl; // je définis la propriéré SRC de l'image avec le contenu de l'imageUrl //
+            hrefImage.href = "product.html?id=" + camera._id;
+    let figcaption = document.createElement("figcaption"); // je crée la balise figcaption //
+
+          
+    
+    titreclasslistcameras.appendChild(titreH2); // j'ajoute le titre h2 à la balise h2 // 
+    newArticleElt.appendChild(titreclasslistcameras); // j'ajoute la balise h2 à la div listcameras //
+    figure.appendChild(hrefImage); // j'ajoute la balise <a> dans la balise figure //
+    hrefImage.appendChild(articleImage); // j'ajoute l'image dans la balise  <a> //
+    newArticleElt.appendChild(figure); // j'ajoute la balise figure à la div listcameras //
+    figure.appendChild(figcaption); // j'ajoute la balise figcaption à la balise figure //
            
-
-            titreclasslistcameras.appendChild(titreH2); // j'ajoute le titre h2 à la balise h2 // 
-            newArticleElt.appendChild(titreclasslistcameras); // j'ajoute la balise h2 à la div listcameras //
-            figure.appendChild(hrefImage); // j'ajoute la balise <a> dans la balise figure //
-            hrefImage.appendChild(articleImage); // j'ajoute l'image dans la balise  <a> //
-            newArticleElt.appendChild(figure); // j'ajoute la balise figure à la div listcameras //
-            (figure.appendChild)(figcaption); // j'ajoute la balise figcaption à la balise figure //
-           
-            figcaption.appendChild(figcaptionpara); // j'ajoute un paragraphe dans la balise figcaption // 
-            figcaptionpara.appendChild(articlePrice); // j'ajoute le prix à la balise figcaptionpara //
-            bodyElt.appendChild(newArticleElt); // j'ajoute l'article au body //
             
 
+mainElt.appendChild(newArticleElt);
+//bodyElt.appendChild(newArticleElt);  j'ajoute l'article au body //
             
-
         }
+        
     }
 };
 request.open("GET", "http://localhost:3000/api/cameras");
---
+++ product.js
@@ -0,0 +1,134 @@
+function getAllUrlParams(url) {
+
+    // obenir la chaine de requête de l'url  (optional) or window 
+    var queryString = url ? url.split('?')[1] : window.location.search.slice(1);
+  
+    // stockage des paramètres ici
+    var obj = {};
+  
+    // si la chaine de requête existe 
+    if (queryString) {
+  
+      // ce qui est après le # de la chaine de requête on oubli 
+      queryString = queryString.split('#')[0];
+  
+      // diviser la chaine de requête en ses composants 
+      var arr = queryString.split('&');
+  
+      for (var i = 0; i < arr.length; i++) {
+        // séparations des clés et des valeurs
+        var a = arr[i].split('=');
+  
+        // définir le nom et la valeur du paramètre (utiliser "true" si vide )
+        var paramName = a[0];
+        var paramValue = typeof (a[1]) === 'undefined' ? true : a[1];
+  
+        // (optional) garder le nom en minuscule
+        paramName = paramName.toLowerCase();
+        if (typeof paramValue === 'string') paramValue = paramValue.toLowerCase();
+  
+        // if the paramName ends with square brackets, e.g. colors[] or colors[2]
+        if (paramName.match(/\[(\d+)?\]$/)) {
+  
+          // create key if it doesn't exist
+          var key = paramName.replace(/\[(\d+)?\]/, '');
+          if (!obj[key]) obj[key] = [];
+  
+          // if it's an indexed array e.g. colors[2]
+          if (paramName.match(/\[\d+\]$/)) {
+            // get the index value and add the entry at the appropriate position
+            var index = /\[(\d+)\]/.exec(paramName)[1];
+            obj[key][index] = paramValue;
+          } else {
+            // ajouter la valeur à la fin du tableau
+            obj[key].push(paramValue);
+          }
+        } else {
+          // nous avons a faire à une chaine 
+          if (!obj[paramName]) {
+            // si n'existe pas , on créé la propriété
+            obj[paramName] = paramValue;
+          } else if (obj[paramName] && typeof obj[paramName] === 'string'){
+            // si la propriété existe et que c'est une chaine , convertir en tableau
+            obj[paramName] = [obj[paramName]];
+            obj[paramName].push(paramValue);
+          } else {
+            // sinon ajouter la propriété 
+            obj[paramName].push(paramValue);
+          }
+        }
+      }
+    }
+  
+    return obj;
+}
+
+window.onload  = function() {
+ 
+    let mon_objet_url = getAllUrlParams();
+    let id = mon_objet_url.id;
+
+console.log(id);
+
+var request = new XMLHttpRequest();
+request.onreadystatechange = function() {
+    if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
+        var camera = JSON.parse(this.responseText);
+        console.log(camera);
+
+        let bodyElt = document.getElementsByTagName("body")[0];   // je récupère l'élément DOM body //
+
+        let headerElt = document.getElementsByTagName("header")[0]; // je récupère l'élément DOM header //
+        let titreHeader = document.createElement("H1"); // je crée une balise h1  // 
+        let textTitrePrincipal = document.createTextNode(camera.name); // je crée  un bloc de texte contenant le titre du header //
+            headerElt.appendChild(titreHeader); // J'attache la balise H1 à l 'header // 
+            titreHeader.appendChild(textTitrePrincipal);  // j'attache le texte à la balise H1 //
+                
+        const newArticleElt = document.createElement("div"); // je crée une balise div //
+        newArticleElt.className = "listcameras"; // je crée une classe dans la balise div //
+        
+        let figure =  document.createElement("figure"); // je crée une balise figure //
+        let hrefImage = document.createElement("a"); // je crée une balise <a> // 
+        let articleImage = document.createElement("IMG"); // je crée une balise image //
+        articleImage.src = camera.imageUrl; // je définis la propriéré SRC de l'image avec le contenu de l'imageUrl //
+        hrefImage.href = "product.html?id=5be1ed3f1c9d44000030b061";
+        let figcaption = document.createElement("figcaption"); // je crée la balise figcaption //
+
+       // let figcaptionpara = document.createElement('p'); // je crée la balise paragraphe //
+        //let articlePrice = document.createTextNode(camera.lenses[0] + " " + "Prix" + " " + camera.price); // je crée un bloc texte contenant le prix de la camera ////
+        //let figcaptionpara2 = document.createElement('p'); // je crée la balise paragraphe /
+        //let articlePrice2 = document.createTextNode(camera.lenses[1] + " " + "Prix" + " " + camera.price);//
+        
+        var selectList = document.createElement("select");
+        selectList.name = "lense";
+        selectList.id = "lense";
+        figcaption.appendChild(selectList);
+        
+        //Create and append the options
+        for (let lense of camera.lenses) {
+            var option = document.createElement("option");
+            option.value = lense;
+            option.text = lense;
+            selectList.appendChild(option);
+        }                
+     
+
+        figure.appendChild(hrefImage); // j'ajoute la balise <a> dans la balise figure //
+        hrefImage.appendChild(articleImage); // j'ajoute l'image dans la balise  <a> //
+        newArticleElt.appendChild(figure); // j'ajoute la balise figure à la div listcameras //
+        (figure.appendChild)(figcaption); // j'ajoute la balise figcaption à la balise figure //
+       
+        //figcaption.appendChild(figcaptionpara); // j'ajoute un paragraphe dans la balise figcaption // 
+        //figcaption.appendChild(figcaptionpara2);
+        //figcaptionpara.appendChild(articlePrice); // j'ajoute le prix à la balise figcaptionpara //
+        //figcaptionpara2.appendChild(articlePrice2); //
+
+        bodyElt.appendChild(newArticleElt); // j'ajoute l'article au body //
+
+    }
+};
+
+request.open( "GET", "http://localhost:3000/api/cameras/" + id); 
+request.send();
+
+}
